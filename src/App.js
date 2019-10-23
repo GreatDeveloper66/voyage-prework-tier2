dbee477fe845034f0e814e753f3866bb4501bc44
Adam <adam@mac.attlocal.net>
2019-10-23 15:03:32 -0500

    Install SASS. Add index.scss. Add display grid mixins

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+/*global window*/
+/*global document*/
+/*global Modernizr*/
+/*global SmoothScroll*/
+/*global console*/
+/*global $*/
+/*jshint esversion:6*/
+/*global event*/
+/*global requestAnimationFrame*/
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <div class = "mainRow" >
+      <div class = "iconTitle"> 
+        Google Fonts 
+      </div> 
+      <div class = "topMenu" >
+<
+    div class = "menuItem>
+      catalog
+
+    <
+    /div> <
+    /div> <
+    /div>
   );
 }
 

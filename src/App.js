ae51075de82ee0f77399ffdcd10281e0b1b52edc
Adam <adam@mac.attlocal.net>
2019-10-24 12:08:31 -0500

    Move react code to class with constructor and state.

--- src/App.js
+++ src/App.js
@@ -8,32 +8,50 @@
 /*global event*/
 /*global requestAnimationFrame*/
 
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
-<div class = "mainRow">
-  <div class = "iconTitle">
-        Google Fonts
-  </div>
-  <div class = "topMenu">
-    <div class = "menuItem">
-      catalog
-    </div>
-    <div class = "menuItem">
-      features
-    </div>
-    <div class = "menuItem">
-      articles
-    </div>
-    <div class = "menuItem">
-      about
-    </div>
-  </div>
-</div>
+    <main/>
   );
 }
+*/
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    };
+
+  }
+  render(){
+    return (
+      <div class = "mainRow">
+        <div class = "iconTitle">
+          Google Fonts
+        </div>
+      <div class = "topMenu">
+        <div class = "menuItem">
+          catalog
+        </div>
+        <div class = "menuItem">
+          features
+        </div>
+        <div class = "menuItem">
+        articles
+        </div>
+        <div class = "menuItem">
+        about
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+
 
 export default App;

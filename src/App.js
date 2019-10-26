fa85cbf5330ca3aadac89fc2fe8c2d3de7305bc1
Adam <adam@mac.attlocal.net>
2019-10-26 09:01:43 -0500

    Rearrange elements in mainRow

--- src/App.js
+++ src/App.js
@@ -9,56 +9,62 @@ class App extends Component {
       data: []
     };
   }
+
   componentDidMount(){
     fetch('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCjVPlNgusY2WdI0-pw303Rt-rIf6YYAVw')
       .then(response => response.json())
       .then(data => {
           this.setState({data: data});
-          console.log(this.state.data.items[0].family);
         });
   }
+
   render(){
+
     return (
       <div className = "App">
         <div className = "mainRow">
           <div className = "iconTitle">
             Google Fonts
-            </div>
+          </div>
           <div className = "topMenu">
-        <div className = "menuItem">
-          catalog
+            <div className = "menuItem">
+              catalog
+            </div>
+            <div className = "menuItem">
+              features
+            </div>
+            <div className = "menuItem">
+              articles
+            </div>
+            <div className = "menuItem">
+              about
+            </div>
+          </div>
         </div>
-        <div className = "menuItem">
-          features
-        </div>
-        <div className = "menuItem">
-        articles
-        </div>
-        <div className = "menuItem">
-        about
-        </div>
-      </div>
-    </div>
     <div className="cards" id="cards">
      {this.state.name}
+     <p>
+      hello
+     </p>
     </div>
-    <Card text={"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipusm."}/>
+    <Card text={"Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipusm."} />
     </div>
     );
   }
 }
 class Card extends Component {
-  constructor(props){
-    super(props);
-  }
-  render() {
+
+
+   render(){
+
     return(
       <div className="card">
+        <p>
         {this.props.text}
+        </p>
       </div>
     );
   }
-
 }
 
 

aaf1d3ad70c2989175858da688810ee0ca168621
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-03 18:54:05 -0500

    Add material-ui-react cards

--- src/Containers/FontCard.js
+++ src/Containers/FontCard.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
+import { Card, CardHeader, CardContent } from '@material-ui/core'
 
 export default class FontCard extends React.Component {
   render() {
-    return (<div className="card" style={{
+    return (<Card style={{
         fontFamily: this.props.fontFamily
       }}>
-      <div className="topCardRow">
+      <CardContent>
         <p>{this.props.fontFamily}</p>
         <button data-toggle="tooltip" data-placement="top" title="Add font family" onClick={() => {this.props.addFontsChosen(this.props.fontFamily)}}>
           <i className="fas fa-plus-circle"></i>
         </button>
-      </div>
-      <div className="cardBody" style={{
+      </CardContent>
+	  <CardContent style={{
           fontSize: this.props.fontSize
         }}>
+     
         {this.props.sampleText}
-      </div>
-    </div>);
+      </CardContent>
+    </Card>);
   }
 }
